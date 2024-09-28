ab40f76b59a724b5e5b3edce48fb72808d1d6810
cherryiJuice <chldlwn711@gmail.com>
2024-09-28 16:53:07 +0900

    feat: 랜덤 숫자 생성

---
+++ java-baseball-6/src/main/java/baseball/util/ComputerUtil.java
@@ -0,0 +1,23 @@
+package baseball.util;
+
+import static baseball.constants.NumberConstants.FIRST_RANGE;
+import static baseball.constants.NumberConstants.INPUT_LENGTH;
+import static baseball.constants.NumberConstants.LAST_RANGE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerUtil {
+
+    public List<Integer> generateNums() {
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < INPUT_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(FIRST_RANGE, LAST_RANGE);
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber);
+            }
+        }
+        return computerNum;
+    }
+}
