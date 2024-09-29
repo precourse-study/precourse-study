d9dee16c1c36fa891495602cbc00164a37d06f6f
cherryiJuice <chldlwn711@gmail.com>
2024-09-29 19:04:20 +0900

    feat: 도메인 클래스 개발

---
+++ java-racingcar-6/src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public static Car of(String name) {
+        return new Car(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public String getPositionDisplay() {
+        return "-".repeat(position);
+    }
+
+}
